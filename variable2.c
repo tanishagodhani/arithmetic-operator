c459f5ce6ab9e94de2add69f297d7da592841f35
tanishagodhani <130463987+tanishagodhani@users.noreply.github.com>
2023-04-11 17:38:35 +0530

    Add files via upload

---
+++ variable2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int a=10;
+	int b=4;
+	int c;
+	c=a+b;
+	printf("Addition of %d&%d=%d\n",a,b,c);
+	c=a-b;
+	printf("substraction of %d&%d=%d\n",a,b,c);
+	c=a*b;
+	printf("multipication of %d&%d=%d\n",a,b,c);
+	c=a/b;
+	printf("division of %d&%d=%d\n",a,b,c);
+	c=a%b;
+	printf("modular of %d&%d=%d\n",a,b,c);
+	
+}
---
+++ variable3.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+main()
+{
+	float a=5.5;
+	float b=4.2;
+	printf(,a+b);
+}
